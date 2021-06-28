e3e5be0152fbb244f03e8a23cabbad731582d070
ArqTras <biuro@ar-net.info>
2021-06-28 10:39:44 +0200

    Stagenet

--- src/i18n/de.js
+++ src/i18n/de.js
@@ -220,7 +220,7 @@ export default {
         rescanSpentOutputs: "Spent Outputs erneut scannen",
         transactionNotes: "Transaktionsnotizen",
         network: "Netzwerk",
-        testnet: "Testnet verwenden"
+        stagenet: "Stagenet verwenden"
     },
     footer: {
         ready: "FERTIG",
