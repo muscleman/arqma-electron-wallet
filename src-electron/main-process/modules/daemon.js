2c823807374cf081a9663ae179f4de4a8e9ceacb
muscleman <musclesonvacation@hotmail.com>
2021-06-29 17:30:38 -0500

    bug fixes

--- src-electron/main-process/modules/daemon.js
+++ src-electron/main-process/modules/daemon.js
@@ -202,10 +202,12 @@ export class Daemon {
             if (options.daemon.type !== "local_zmq") {
                 this.daemonProcess.stdout.on("data", data => process.stdout.write(`Daemon: ${data}`))
 
+                let daemon_info = {}
                 // To let caller know when the daemon is ready
                 let intrvl = setInterval(async() => {
                     try {
-                        const result = await this.rpcDaemon.getInfo()
+                        daemon_info.info = await this.rpcDaemon.getInfo()
+                        this.sendGateway("set_daemon_data", daemon_info)
                         clearInterval(intrvl)
                         this.startHeartbeat()
                         resolve()
@@ -340,11 +342,12 @@ export class Daemon {
                 blockHeaderByHeightData = await this.rpcDaemon.getLastBlockHeader()
                 }
                 catch (error) {
+                    console.log(`daemon.timestampToHeight ${error}`)
                     return reject()
                 }
             }
 
-            let new_pivot = [blockHeaderByHeightData.result.block_header.height, blockHeaderByHeightData.result.block_header.timestamp]
+            let new_pivot = [blockHeaderByHeightData.block_header.height, blockHeaderByHeightData.block_header.timestamp]
 
             // If we are within an hour that is good enough
             // If for some reason there is a > 1h gap between blocks
